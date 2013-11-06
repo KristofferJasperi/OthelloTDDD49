e1d397a65a74762cee464ce9d8dc552a8ab68aa2
KristofferJasperi <kristoffer89j@gmail.com>
2013-11-06 23:40:59 +0100

    First

---
+++ Othello/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Othello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonNewGame_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("hej");
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("nop");
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hej då");
+            Application.Current.Shutdown();
+        }
+
+
+    }
+}
